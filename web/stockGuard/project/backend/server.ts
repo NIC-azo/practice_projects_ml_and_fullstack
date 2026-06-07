d88fb04ee7f05482e5b849db9ce4e54a89e4e73c
NIC-azo <soyfrancesco37@gmail.com>
2026-06-06 22:45:31 -0500

    feat: seteo de el server.ts al 60%

---
+++ web/stockGuard/project/backend/server.ts
@@ -0,0 +1,25 @@
+import express from "express"
+import cors from "cors"
+import "dotenv/config"
+
+const environment = process.env["NODE_ENV"] || "";
+
+const app = express();
+
+app.use(cors({
+    credentials: true,
+    methods: ['GET,HEAD,PUT,PATCH,POST,DELETE'],
+    origin: process.env["FRONTEND_LOCAL"] || ""
+}));
+
+app.use(express.json());
+
+app.use(express.urlencoded({
+    extended: true,
+}));
+
+const DISCERN_PORT = environment === "dev" ? process.env["LOCAL_PORT"] || "" : process.env["PORT"] || "";
+
+app.listen(DISCERN_PORT, () => {
+    console.log(`servidor funcionando en puerto: `, DISCERN_PORT);
+});
