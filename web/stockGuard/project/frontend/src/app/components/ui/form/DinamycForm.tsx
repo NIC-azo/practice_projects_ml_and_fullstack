907a02a8c8c63494e9ef4325e147dccec384dacc
NIC-azo <soyfrancesco37@gmail.com>
2026-06-24 23:04:27 -0500

    feat: frontend avanzado al 7.4

--- web/stockGuard/project/frontend/src/app/components/ui/form/DinamycForm.tsx
+++ web/stockGuard/project/frontend/src/app/components/ui/form/DinamycForm.tsx
@@ -1,16 +1,49 @@
-import { useState, type Dispatch, type SetStateAction } from "react";
-import type { FormType } from "@/types/typos.bd";
+import { useEffect, type Dispatch, type SetStateAction } from "react";
 
-function DinamycForm(
-    {formType, handleForm, activated, setActivated}:
+const DinamycForm = (
+    {children, title, isOpen, setIsOpen, size}:
     {
-        formType: FormType;
-        handleForm: (e: SubmitEvent) => void;
-        activated: boolean;
-        setActivated: Dispatch<SetStateAction<boolean>>;
+        children: React.ReactNode;
+        title: string;
+        isOpen: boolean;
+        setIsOpen: Dispatch<SetStateAction<boolean>>;
+        size?: 'md' | 'sm' | 'lg' | 'xl'
     }
-) {
-    let 
+) => {
+
+    useEffect(() => {
+        const handleEscape = (e: KeyboardEvent) => {
+            if (e.key === "Escape") setIsOpen(false);
+        };
+        if (isOpen) {
+            document.addEventListener('keydown', handleEscape);
+            document.body.style.overflow = 'hidden'
+        }
+        return () => {
+            document.removeEventListener('keydown', handleEscape);
+            document.body.style.overflow = 'unset';
+        };
+    }, [isOpen, setIsOpen]);
+
+    if (!isOpen) return null;
+    const sizeClasses = {
+        sm: 'max-w-md',
+        md: 'max-w-lg',
+        lg: 'max-w-2xl',
+        xl: 'max-w-4xl',
+    }
+
+    return (
+        <div className="inset-0 fixed z-50 flex items-center justify-center p-4">
+            <div
+                className="absolute inset-0 bg-black/50 backdrop-blur-sm"
+                onClick={() => setIsOpen(false)} />
+            <div
+                className={`relative bg-background-dark`}>
+
+            </div>
+        </div>
+    );
 }
 
 export default DinamycForm;
