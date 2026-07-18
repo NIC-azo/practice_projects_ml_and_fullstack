e0fa31502b42e04aa1ca9ec8a59e0b962505c62a
NIC-azo <soyfrancesco37@gmail.com>
2026-07-18 17:16:44 -0500

    feat: frontend avanzado al 51%, products page casi terminado, uso de useLocation y useNavigate en App

--- web/stockGuard/project/frontend/src/App.tsx
+++ web/stockGuard/project/frontend/src/App.tsx
@@ -1,19 +1,51 @@
-import { BrowserRouter } from "react-router-dom"
-import AppRouter from "@/router/index"
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools"
-
-const queryClient = new QueryClient();
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { BrowserRouter, useNavigate, useLocation } from "react-router-dom";
+import { useAuthStore } from "@/store/auth.store";
+import AppRouter from "@/router/index";
+import {
+  MutationCache,
+  QueryCache,
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 function App() {
+  const navigate = useNavigate();
+  const location = useLocation();
+  const { logout } = useAuthStore();
+
+  const queryClient = new QueryClient({
+    queryCache: new QueryCache({
+      onError: (error: any) => {
+        if (error.status === 401) {
+          logout();
+          navigate("/logout", { state: { from: location }, replace: true });
+        } else if (error.status === 403) {
+          navigate("/unauthorized", { replace: true });
+        }
+      },
+    }),
+    mutationCache: new MutationCache({
+      onError: (error: any) => {
+        if (error.status === 401) {
+          logout();
+          navigate("/logout", { state: { from: location }, replace: true });
+        } else if (error.status === 403) {
+          navigate("/unauthorized", { replace: true });
+        }
+      },
+    }),
+  });
+
   return (
     <BrowserRouter>
       <QueryClientProvider client={queryClient}>
         <AppRouter />
-        <ReactQueryDevtools initialIsOpen={false}/>
+        <ReactQueryDevtools initialIsOpen={false} />
       </QueryClientProvider>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
--- web/stockGuard/project/frontend/src/app/pages/ProductsPage.tsx
+++ web/stockGuard/project/frontend/src/app/pages/ProductsPage.tsx
@@ -9,7 +9,6 @@ import { useMemo, useState } from "react";
 import DinamycForm from "@/app/components/ui/form/DinamycForm";
 import { useQuery, useQueryClient, useMutation } from "@tanstack/react-query";
 import { useAuthStore } from "@/store/auth.store";
-import { Navigate, useLocation } from "react-router-dom";
 
 const initialProductValues: CreateProduct = {
   name: "",
@@ -41,7 +40,6 @@ const ProductsPage = () => {
   });
   const [formData, setFormData] = useState<CreateProduct>(initialProductValues);
   const { user } = useAuthStore();
-  const location = useLocation();
   const [active, setActive] = useState<boolean>(false);
   const [isEditing, setIsEditing] = useState<boolean>(false);
   const [editingId, setEditingId] = useState<string | null>(null);
@@ -71,15 +69,13 @@ const ProductsPage = () => {
     },
     onError: (error) => {
       if (error.isNetworkError) {
-        setFormError("Error de conexion con el servidor, intentelo mas tarde")
+        setFormError("Error de conexion con el servidor, intentelo mas tarde");
       } else if (error.status === 401) {
         setFormError("su sesion a expirado, intente iniciar sesion");
-        <Navigate to={"/login"} state={{from: location}} replace/>
       } else if (error.status === 403) {
         setFormError("no estas permitido para realizar esta accion");
-        <Navigate to={"/unauthorized"} replace/>
       } else {
-        setFormError(error.message)
+        setFormError(error.message);
       }
     },
   });
@@ -95,7 +91,17 @@ const ProductsPage = () => {
       queryClient.invalidateQueries({ queryKey: ["products"] });
       clearFields();
     },
-    onError: (error) => {setFormError(error.message)},
+    onError: (error) => {
+      if (error.isNetworkError) {
+        setFormError("Error de conexion con el servidor, intentelo mas tarde");
+      } else if (error.status === 401) {
+        setFormError("su sesion a expirado, intente iniciar sesion");
+      } else if (error.status === 403) {
+        setFormError("no estas permitido para realizar esta accion");
+      } else {
+        setFormError(error.message);
+      }
+    },
   });
 
   const increaseStockHandler = useMutation<
@@ -109,7 +115,17 @@ const ProductsPage = () => {
       queryClient.invalidateQueries({ queryKey: ["products"] });
       clearFields();
     },
-    onError: (error) => {setFormError(error.message)},
+    onError: (error) => {
+      if (error.isNetworkError) {
+        setFormError("Error de conexion con el servidor, intentelo mas tarde");
+      } else if (error.status === 401) {
+        setFormError("su sesion a expirado, intente iniciar sesion");
+      } else if (error.status === 403) {
+        setFormError("no estas permitido para realizar esta accion");
+      } else {
+        setFormError(error.message);
+      }
+    },
   });
 
   const deleteProductHandler = useMutation<
@@ -122,12 +138,24 @@ const ProductsPage = () => {
       queryClient.invalidateQueries({ queryKey: ["products"] });
       clearFields();
     },
-    onError: (error) => {setFormError(error.message)},
+    onError: (error) => {
+      if (error.isNetworkError) {
+        setFormError("Error de conexion con el servidor, intentelo mas tarde");
+      } else if (error.status === 401) {
+        setFormError("su sesion a expirado, intente iniciar sesion");
+      } else if (error.status === 403) {
+        setFormError("no estas permitido para realizar esta accion");
+      } else {
+        setFormError(error.message);
+      }
+    },
   });
 
   const handleSubmit = (e: React.SubmitEvent<HTMLFormElement>) => {
     e.preventDefault();
     setFormError("");
+
+    if (handleModal === "view") return;
 
     if (editingId && handleModal === "increase_stock") {
       if ((formData.current_stock ?? 0) <= 0) {
@@ -143,12 +171,12 @@ const ProductsPage = () => {
 
     const formEnhanced = {
       ...formData,
-      expiration_date:
-        formData.expiration_date ??
-        new Date(formData.expiration_date).toISOString(),
+      expiration_date: formData.expiration_date
+        ? new Date(formData.expiration_date).toISOString()
+        : "",
     };
 
-    if (editingId && isEditing) {
+    if (editingId && (handleModal === "edit" || isEditing)) {
       updateProductHandler.mutate({ id: editingId, data: formEnhanced });
     } else {
       createProductHandler.mutate(formEnhanced);
@@ -202,26 +230,33 @@ const ProductsPage = () => {
       : "la actualizacion de un producto esta tomando mucho tiempo, por favor espere o recargue la pagina",
   };
 
-  if (isLoading){ 
-    return <div></div>;
+  if (isLoading) {
+    return (
+      <div className="">
+        <p className="">cargando...</p>
+      </div>
+    );
   }
 
   return (
     <div className="space-y-5 font-sans">
-      {isError && formError.length > 0 && 
-        <div className="fixed inset-0 z-50 flex items-center justify-center bg-black/50 dark:bg-black/75 p-4 
+      {isError && formError.length > 0 && (
+        <div
+          className="fixed inset-0 z-50 flex items-center justify-center bg-black/50 dark:bg-black/75 p-4 
           animate-fade-in-form-modal duration-200"
-          >
-            <div className="relative w-full max-w-md bg-background-emojis-color-alert p-6 rounded-2xl shadow-blur-for-shadows">
-              <button type="button" onClick={() => setFormError("")}>
-                <i className="fa-solid fa-square-xmark text-xl text-background-emojis-color-alert hover:text-background-emojis-color-alert/60"/>
-              </button>
-              <div className="pr-6">
-                <p className="text-lg font-bold text-color-text-general leading-snug">{formError}</p>
-              </div>
+        >
+          <div className="relative w-full max-w-md bg-background-emojis-color-alert p-6 rounded-2xl shadow-blur-for-shadows">
+            <button type="button" onClick={() => setFormError("")}>
+              <i className="fa-solid fa-square-xmark text-xl text-background-emojis-color-alert hover:text-background-emojis-color-alert/60" />
+            </button>
+            <div className="pr-6">
+              <p className="text-lg font-bold text-color-text-general leading-snug">
+                {formError}
+              </p>
             </div>
+          </div>
         </div>
-      }
+      )}
       {editingId && (
         <div className="fixed inset-0 z-50 flex items-center justify-center p-4 overflow-hidden">
           <form onSubmit={handleSubmit} className="space-y-4">
@@ -340,21 +375,23 @@ const ProductsPage = () => {
                       Fecha de expiracion
                     </label>
                     {handleModal === "view" ? (
-                      <p>{formData.expiration_date}</p>
-                    ) : (
-                      <input
-                        className="w-full px-3 py-2.5 pr-10 rounded-lg text-sm border outline-none transition-all bg-background-buttons
-                                border-sky-500 text-color-text-button/80"
-                        value={Intl.DateTimeFormat("es-PE", {
+                      <p>
+                        {Intl.DateTimeFormat("es-PE", {
                           day: "2-digit",
                           month: "2-digit",
                           year: "numeric",
                           timeZone: "UTC",
                         }).format(new Date(formData.expiration_date))}
+                      </p>
+                    ) : (
+                      <input
+                        className="w-full px-3 py-2.5 pr-10 rounded-lg text-sm border outline-none transition-all bg-background-buttons
+                                border-sky-500 text-color-text-button/80"
+                        value={formData.expiration_date}
                         onChange={(e) =>
                           setFormData({
                             ...formData,
-                            lote: e.target.value,
+                            expiration_date: e.target.value,
                           })
                         }
                         type="date"
@@ -505,9 +542,12 @@ const ProductsPage = () => {
               {handleModal === "view" && user?.rol === "ADMIN" && (
                 <button
                   type="button"
-                  onClick={() => setHandleModal("edit")}
+                  onClick={() => {
+                    setIsEditing(true);
+                    setHandleModal("edit");
+                  }}
                 >
-                  <i className="fa-solid fa-pen-to-square text-background-emojis-color"/>
+                  <i className="fa-solid fa-pen-to-square text-background-emojis-color" />
                   Editar
                 </button>
               )}
@@ -527,7 +567,7 @@ const ProductsPage = () => {
                         : "una accion llevada a cabo recientemente esta tomando mas tiempo, reinicie el sistema o contacte soporte tecnico"
                   }
                 >
-                  <i className="fa-solid fa-circle-check text-background-emojis-color"/>
+                  <i className="fa-solid fa-circle-check text-background-emojis-color" />
                   Guardar Cambios
                 </button>
               )}
@@ -649,15 +689,29 @@ const ProductsPage = () => {
                   </div>
 
                   <div className="flex items-center justify-center gap-2">
-                    <button type="button" aria-label="ver mas" onClick={() => handleOpenModal(p, "view")}>
+                    <button
+                      type="button"
+                      aria-label="ver mas"
+                      onClick={() => handleOpenModal(p, "view")}
+                    >
                       <i className="fa-solid fa-ellipsis text-background-emojis-color" />
                     </button>
-                    <button type="button" aria-label="incrementar stock" onClick={() => handleOpenModal(p, "increase_stock")}>
+                    <button
+                      type="button"
+                      aria-label="incrementar stock"
+                      onClick={() => handleOpenModal(p, "increase_stock")}
+                    >
                       <i className="fa-solid fa-angles-up text-background-emojis-color" />
                       stock
                     </button>
                     {user?.rol === "ADMIN" && (
-                      <button type="button" aria-label="eliminar producto" onClick={() => deleteProductHandler.mutate({id: p.id})}>
+                      <button
+                        type="button"
+                        aria-label="eliminar producto"
+                        onClick={() =>
+                          deleteProductHandler.mutate({ id: p.id })
+                        }
+                      >
                         <i className="fa-solid fa-trash-can text-background-emojis-color-alert" />
                       </button>
                     )}
@@ -668,9 +722,208 @@ const ProductsPage = () => {
           )}
         </div>
       </div>
-      <DinamycForm isOpen={active} setIsOpen={setActive} title="">
-        <form onSubmit={handleSubmit}>
-
+      <DinamycForm
+        isOpen={active}
+        setIsOpen={setActive}
+        title="Crear Producto"
+        size="lg"
+      >
+        <form className="space-y-4" onSubmit={handleSubmit}>
+          <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
+            <div className="font-medium">
+              <label className="block text-sm text-color-text-general mb-1">
+                Nombre *
+              </label>
+              <input
+                type="text"
+                onChange={(e) =>
+                  setFormData({ ...formData, name: e.target.value })
+                }
+                value={formData.name}
+                className="w-full px-3 py-2.5 pr-10 rounded-lg text-sm border outline-none transition-all bg-background-buttons
+                border-sky-500 text-color-text-button/80"
+              />
+            </div>
+            <div className="font-medium">
+              <label className="block text-sm text-color-text-general mb-1">
+                Codigo de Barras
+              </label>
+              <input
+                className="w-full px-3 py-2.5 pr-10 rounded-lg text-sm border outline-none transition-all bg-background-buttons
+                                border-sky-500 text-color-text-button/80"
+                value={formData.bars_code}
+                onChange={(e) =>
+                  setFormData({
+                    ...formData,
+                    bars_code: e.target.value,
+                  })
+                }
+                type="text"
+                placeholder="codigo de barra"
+              />
+            </div>
+            <div>
+              <label className="block text-sm font-medium text-color-text-general mb-1">
+                Lote
+              </label>
+              <input
+                className="w-full px-3 py-2.5 pr-10 rounded-lg text-sm border outline-none transition-all bg-background-buttons
+                                border-sky-500 text-color-text-button/80"
+                value={formData.lote ?? ""}
+                onChange={(e) =>
+                  setFormData({
+                    ...formData,
+                    lote: e.target.value,
+                  })
+                }
+                type="text"
+                placeholder="lote"
+              />
+            </div>
+            <div>
+              <label className="block text-sm font-medium text-color-text-general mb-1">
+                Fecha de expiracion
+              </label>
+              <input
+                className="w-full px-3 py-2.5 pr-10 rounded-lg text-sm border outline-none transition-all bg-background-buttons
+                                border-sky-500 text-color-text-button/80"
+                value={formData.expiration_date}
+                onChange={(e) =>
+                  setFormData({
+                    ...formData,
+                    expiration_date: e.target.value,
+                  })
+                }
+                type="date"
+                placeholder="formato (YY/MM/DD)"
+              />
+            </div>
+            <div>
+              <label className="block text-sm font-medium text-color-text-general mb-1">
+                Precio Unitario
+              </label>
+              <input
+                className="w-full px-3 py-2.5 pr-10 rounded-lg text-sm border outline-none transition-all bg-background-buttons
+                                border-sky-500 text-color-text-button/80"
+                value={formData.unity_price.toFixed(2)}
+                onChange={(e) =>
+                  setFormData({
+                    ...formData,
+                    unity_price: Number(e.target.value),
+                  })
+                }
+                type="number"
+                placeholder="ingrese precio unitario"
+              />
+            </div>
+            <div>
+              <label className="block text-sm font-medium text-color-text-general mb-1">
+                limite de adquisicion por menor (opcional)
+              </label>
+              <input
+                className="w-full px-3 py-2.5 pr-10 rounded-lg text-sm border outline-none transition-all bg-background-buttons
+                                border-sky-500 text-color-text-button/80"
+                value={formData.limit_minor_adquirition}
+                onChange={(e) =>
+                  setFormData({
+                    ...formData,
+                    limit_minor_adquirition: Number(e.target.value),
+                  })
+                }
+                type="number"
+                placeholder="ingrese limite de adquisicion de producto"
+              />
+            </div>
+            <div>
+              <label className="block text-sm font-medium text-color-text-general mb-1">
+                Precio al por Menor
+              </label>
+              <input
+                className="w-full px-3 py-2.5 pr-10 rounded-lg text-sm border outline-none transition-all bg-background-buttons
+                                border-sky-500 text-color-text-button/80"
+                value={formData.minorsale_price}
+                onChange={(e) =>
+                  setFormData({
+                    ...formData,
+                    minorsale_price: Number(e.target.value),
+                  })
+                }
+                type="number"
+                placeholder="ingrese el precio al por menor"
+              />
+            </div>
+            <div>
+              <label className="block text-sm font-medium text-color-text-general mb-1">
+                Precio al por Mayor
+              </label>
+              <input
+                className="w-full px-3 py-2.5 pr-10 rounded-lg text-sm border outline-none transition-all bg-background-buttons
+                                border-sky-500 text-color-text-button/80"
+                value={formData.wholesale_price}
+                onChange={(e) =>
+                  setFormData({
+                    ...formData,
+                    wholesale_price: Number(e.target.value),
+                  })
+                }
+                type="number"
+                placeholder="ingrese el precio al por mayor"
+              />
+            </div>
+            <div>
+              <label className="block text-sm font-medium text-color-text-general mb-1">
+                Stock Actual (opcional)
+              </label>
+              <input
+                className="w-full px-3 py-2.5 pr-10 rounded-lg text-sm border outline-none transition-all bg-background-buttons
+                                border-sky-500 text-color-text-button/80"
+                value={formData.current_stock}
+                onChange={(e) =>
+                  setFormData({
+                    ...formData,
+                    current_stock: Number(e.target.value),
+                  })
+                }
+                type="number"
+                placeholder="ingrese stock actual (esto no incrementa el stock)"
+              />
+            </div>
+            <div>
+              <label className="block text-sm font-medium text-color-text-general mb-1">
+                Stock Minimo (opcional)
+              </label>
+              <input
+                className="w-full px-3 py-2.5 pr-10 rounded-lg text-sm border outline-none transition-all bg-background-buttons
+                                border-sky-500 text-color-text-button/80"
+                value={formData.minimun_stock}
+                onChange={(e) =>
+                  setFormData({
+                    ...formData,
+                    minimun_stock: Number(e.target.value),
+                  })
+                }
+                type="number"
+                placeholder="ingrese un minimo para diferenciar el stock (esto no incrementa el stock)"
+              />
+            </div>
+          </div>
+          <div className="flex justify-center gap-2">
+            <button
+              type="submit"
+              className="px-4 py-2 border text-background-emojis-color rounded-lg bg-background-buttons hover:bg-background-buttons/70"
+            >
+              <i className="fa-solid fa-floppy-disk text-lg" />
+              Guardar
+            </button>
+            <button
+              type="button"
+              className="px-4 py-2 text-background-emojis-color-alert"
+              onClick={() => setActive(false)}
+            >
+              <i className="fa-solid fa-ban text-lg" />
+              Cancelar
+            </button>
+          </div>
         </form>
       </DinamycForm>
     </div>
